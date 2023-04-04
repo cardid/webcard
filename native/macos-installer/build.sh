d1f3a158ee92e01a1fa631ca5af6a349cc2e95e4
Adrian Castillo <acastillo@hidglobal.com>
2023-04-04 18:08:45 +0200

    Cleanup after building installer

--- native/macos-installer/build.sh
+++ native/macos-installer/build.sh
@@ -5,3 +5,4 @@ cp ../org.cardid.webcard.native.json ./files/Library/Application\ Support/CardID
 pkgbuild --root ./files --identifier WebCard --version 0.4.0 --install-location "/" --scripts ./scripts intermediate.pkg
 productbuild --distribution distribution.xml --package-path intermediate.pkg webcard.pkg
 rm intermediate.pkg
+rm -rf ./files
